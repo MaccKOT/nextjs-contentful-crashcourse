f2ad5dc4b08b5b620c8f5e7dd5407dfa56c62976
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-04-13 13:14:47 +0400

    Add get data from slug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,45 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_API_KEY,
+}); //connection to Contentful space
+
+//paths generator
+export async function getStaticPaths() {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//single item generator
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug,
+  });
+
+  return {
+    props: {
+      recipe: items[0],
+    },
+  }; //return only fist item
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log('recipe >> ', recipe);
+  return <div>Recipe Details</div>;
 }

80b27a7f0791df5f16f3f0cf93cd7db94acbc3bc
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-04-13 12:00:20 +0400

    Add connection to Contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_API_KEY,
+  }); //connection to Contentful space
+
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+
+  return <div className='recipe-list'>Recipe List</div>;
 }
